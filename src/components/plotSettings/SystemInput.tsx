0c078723274104d29c5fb278169bb456109c9fdb
William Grant <williamg3575467@gmail.com>
2024-09-22 19:23:17 +0100

    Set up basic auto complete for system names

--- src/components/plotSettings/SystemInput.tsx
+++ src/components/plotSettings/SystemInput.tsx
@@ -1,14 +1,24 @@
+import axios from "axios"
 import { useState } from "react"
 
 const SystemInput = ({ placeholder }: { placeholder: string }) => {
   const [value, setValue] = useState("")
+  const [autoComplete, setAutoComplete] = useState<string[]>([])
 
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const changeHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
+    if (e.target.value.length < 3) return setAutoComplete([]) // Prevents unnecessary requests
+    const res = await axios.get(`http://localhost:3000/systems?q=${e.target.value}`)
+    setAutoComplete(res.data)
   }
 
   return (
-    <input type="text" value={value} onChange={changeHandler} placeholder={placeholder}/>
+    <>
+      <input type="text" value={value} onChange={changeHandler} placeholder={placeholder}/>
+      <ul>
+        {autoComplete && autoComplete.map((system) => <li key={system}>{system}</li>)}
+      </ul>
+    </>
   )
 }
 
